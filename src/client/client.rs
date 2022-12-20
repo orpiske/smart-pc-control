73d52da5eec86b1472a32df3d4638aa45ebe7119
Otavio R. Piske <angusyoung@gmail.com>
2022-12-20 10:18:20 +0100

    WIP: some cleanups to the subscript / state on logic

--- src/client/client.rs
+++ src/client/client.rs
@@ -2,8 +2,31 @@ use std::process;
 use std::time::Duration;
 
 use paho_mqtt as mqtt;
+use paho_mqtt::Client;
 
 const DEFAULT_QOS: i32 = 1;
+const QOS_AT_MOST_ONCE: i32 = mqtt::QOS_1;
+
+fn clean_disconnect(cli: &Client) {
+    // Disconnect from the broker
+    cli.disconnect(None).unwrap();
+}
+
+fn set_turned_on_state(default_topic: &str, cli: &Client) {
+    // Create a message and publish it
+    let msg = mqtt::MessageBuilder::new()
+        .topic(default_topic)
+        .payload("true")
+        .qos(DEFAULT_QOS)
+        .finalize();
+
+    if let Err(e) = cli.publish(msg) {
+        println!("Error sending message: {:?}", e);
+
+        clean_disconnect(&cli);
+        process::exit(1);
+    }
+}
 
 pub fn client(address: String) {
     let default_topic = "pc/nuc/status/on";
@@ -12,6 +35,8 @@ pub fn client(address: String) {
         println!("Error creating the client: {:?}", err);
         process::exit(1);
     });
+
+    let rx = cli.start_consuming();
 
     let conn_opts = mqtt::ConnectOptionsBuilder::new()
         .keep_alive_interval(Duration::from_secs(20))
@@ -25,6 +50,18 @@ pub fn client(address: String) {
                     "Connected to: '{}' with MQTT version {}",
                     conn_rsp.server_uri, conn_rsp.mqtt_version
                 );
+
+                println!("Subscribing to {default_topic}");
+                cli.subscribe(default_topic, QOS_AT_MOST_ONCE)
+                    .and_then(|rsp| {
+                        rsp.subscribe_response().ok_or(mqtt::Error::General("Unable to subscribe due to Bad MQTT response"))
+                    })
+                    .unwrap_or_else(|err| {
+                        println!("Unable to subscribe to topic: {:?}", err);
+                        clean_disconnect(&cli);
+                        process::exit(1);
+                    });
+
             }
         }
         Err(e) => {
@@ -33,18 +70,31 @@ pub fn client(address: String) {
         }
     }
 
-    // Create a message and publish it
-    let msg = mqtt::MessageBuilder::new()
-        .topic(default_topic)
-        .payload("true")
-        .qos(DEFAULT_QOS)
-        .finalize();
+    set_turned_on_state(default_topic, &cli);
 
-
-    if let Err(e) = cli.publish(msg) {
-        println!("Error sending message: {:?}", e);
+    for msg in rx.iter() {
+        if let Some(msg) = msg {
+            match msg.payload_str().trim().parse() {
+                Ok(v) => {
+                    dbg!("Received a request with data: {v}");
+                    let data: String = v;
+                    dbg!("Received a parseable response as string: {data}");
+                    if data.eq("true") {
+                        println!("The computer is already on");
+                    } else if data.eq("false") {
+                        println!("Shutting down the computer ...");
+                    } else {
+                        println!("Invalid request data");
+                    }
+                }
+                Err(_) => {
+                    println!("Failed to parse the response");
+                    continue;
+                }
+            }
+        }
     }
 
-    // Disconnect from the broker
-    cli.disconnect(None).unwrap();
+    clean_disconnect(&cli);
 }
+
--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,6 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let address = &args[1];
 
-    client::run_from_mod2();
-    // client::client::run_from_mod();
     let address = String::from(address);
     client::client::client(address);
 
