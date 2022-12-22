f0892e07c087f4707f3860dbdd753960a3e7857c
Otavio R. Piske <angusyoung@gmail.com>
2022-12-22 09:02:50 +0100

    WIP: added builtin wake-on-lan support

--- src/power_control/power_control.rs
+++ src/power_control/power_control.rs
@@ -1,11 +1,13 @@
+use std::env::VarError;
 use paho_mqtt as mqtt;
 use paho_mqtt::{Client, Message};
+use wake_on_lan as wol;
 
 const QOS_AT_MOST_ONCE: i32 = mqtt::QOS_1;
 
 use system_shutdown::shutdown;
 
-fn turn_off() {
+fn turn_off() -> () {
     let default_environment = String::from("development");
     let environment= std::env::var("SMART_PC_CONTROL_ENVIRONMENT").unwrap_or(default_environment);
 
@@ -16,6 +18,41 @@ fn turn_off() {
         }
     } else {
         println!("The application is running on {environment} environment where shutdown is not allowed. Set SMART_PC_CONTROL_ENVIRONMENT to 'production' to override")
+    }
+}
+
+fn turn_on() {
+    let mut mac_address: [u8; 6] = [0; 6];
+
+    match std::env::var("SMART_PC_CONTROL_TARGET_MAC_ADDRESS") {
+        Ok(address) => {
+            let mut i: i8 = 0;
+            for octet in address.unwrap().split(':') {
+                match octet.parse() {
+                    Ok(octet) => {
+                        mac_address[i] = octet;
+                        i += 1;
+                    }
+                    Err(error) => {
+                        println("Unable to convert value to octet: {}", error);
+                        break;
+                    }
+                };
+            }
+        }
+        Err(_) => {
+            eprintln!("The target mac address is unset. Set SMART_PC_CONTROL_TARGET_MAC_ADDRESS to configure")
+            return;
+        }
+    };
+
+    let magic_packet = wol::MagicPacket::new(&mac_address);
+
+    match magic_packet.send() {
+        Ok(_) => { println!("Magic packet sent successfully"); }
+        Err(error) => {
+            println!("Unable to send magic packet: {}", error);
+        }
     }
 }
 
