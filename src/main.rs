ae0542395a77c5fd762cfb4c60d1c04ce951c71b
Otavio R. Piske <angusyoung@gmail.com>
2022-12-22 11:10:11 +0100

    WIP: command cleanups

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 // use crate::power_control
 
+extern crate core;
+
 use std::env;
 
 pub mod client;
@@ -7,7 +9,21 @@ pub mod power_control;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let address = &args[1];
+
+    if args.len() != 3 {
+        panic!("Invalid arguments: use {} [power] [mqtt-broker-address]", &args[0]);
+    }
+
+    if args[1].eq("power") {
+        return power_control();
+    }
+
+    panic!("Invalid command: {}", &args[1])
+}
+
+fn power_control() {
+    let args: Vec<String> = env::args().collect();
+    let address = &args[2];
 
     let address = String::from(address);
 
